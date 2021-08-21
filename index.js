05bad940e1f6fd48a7fd7cd0103d9daf8455cd69
Meheknaz1704 <meheknaz258@gmail.com>
2021-08-21 09:26:51 +0530

    dom

--- index.js
+++ index.js
@@ -18,3 +18,31 @@ for(i=0;i<head .length;i++){
     console.log(head[i])
 
 }
+
+let parel=document.getElementById("parent")
+console.log(parel)
+
+
+let par=document.getElementById("parent")
+console.log(par.lastChild);
+
+let pa=document.getElementsById("parent")
+console.log(pa.firstChlid);
+
+let parent=document.getElementById("parent")
+console.log(parent.children)
+
+let midpara=document.getElementsById("midpara")
+console.log(midpara.nextElementSibling);
+
+let mid=document.getElementsById("midpara")
+console.log(mid.previousElementSiblings)
+
+let mid1=document.getElementsById("midpara")
+console.log(mid1.nextSibling)
+
+let mid2=document.getElementsById("midpara")
+console.log(mid2.previousSibling)
+
+
+
